1bdca68929654c7f1f0fd3f9344cb27bc06c12e1
Iwerson <109252776+owleyes62@users.noreply.github.com>
2026-02-27 22:48:25 +0000

    atividade 2 concluida

--- Atividade_2/app/index.tsx
+++ Atividade_2/app/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import {Text, StyleSheet, TextInput} from 'react-native';
+import {Text, StyleSheet, TextInput, View} from 'react-native';
 import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
 
 const TextInputExample = () => {
   const [number, onChangeNumber] = React.useState('');
+  const [number2, onChangeNumber2] = React.useState('');
+  const [number3, onChangeNumber3] = React.useState('');  
   const today = new Date();
+  const data = new Date(today.getFullYear(), Number(number3) - 1, Number(number2));
 
   return (
     <SafeAreaProvider>
@@ -17,11 +20,27 @@ const TextInputExample = () => {
           onChangeText={onChangeNumber}
           value={number}
         />
-    
+        <Text>{'\n'}Aniversario</Text>
+        <View style={styles.containerHorizontal}>
+          <TextInput
+            style={styles.Aniversario}
+            placeholder="Dia"
+            onChangeText={onChangeNumber2}
+            value={number2}
+          />
+
+          <TextInput
+            style={styles.Aniversario}
+            placeholder="Mes"
+            onChangeText={onChangeNumber3}
+            value={number3}
+          />
+        </View>
+
         <Text>Seu ano de nascimento é:</Text>
         <TextInput
           style={styles.input}
-          value={String(today.getFullYear() - Number(number))}
+          value={String(today.getFullYear() - Number(number) - (data > today ? 1 : 0))}
           
         />
       </SafeAreaView>
@@ -34,12 +53,22 @@ const styles = StyleSheet.create({
     fontSize: 42,
   },
 
+  containerHorizontal: {
+    flexDirection: 'row',
+  },
+
+  Aniversario: {
+    fontSize: 20,
+    height: 45,
+    margin: 10,
+    borderWidth: 1,
+  },
+
   input: {
     fontSize: 20,
     height: 45,
     margin: 10,
     borderWidth: 1,
-   
   },
 });
 
